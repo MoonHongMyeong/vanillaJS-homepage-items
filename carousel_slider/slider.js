3d482dca7531622e42013ef233b8ca08646c8153
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-02-03 10:43:35 +0900

    소스안에 주석 제거

--- carousel_slider/slider.js
+++ carousel_slider/slider.js
@@ -15,12 +15,10 @@ export class Slider {
         const width = root.getAttribute(`width`);
         const height = root.getAttribute(`height`);
 
-        // 슬라이드 컨테이너 생성
         const sliderContainer = document.createElement(`div`);
         sliderContainer.className = 'custom-slider-container';
         sliderContainer.style = `width:${width}; height:${height}`;
 
-        // 슬라이드 아이템들 생성
         this._items.forEach((item, index) => {
             const sliderItem = document.createElement(`div`);
             sliderItem.className = `custom-slider-item${item.id}`;
@@ -40,7 +38,6 @@ export class Slider {
             sliderContainer.appendChild(sliderItem);
         })
 
-        // 버튼 기능
         const leftBtn = document.createElement(`button`);
         leftBtn.id = `slider-left-btn`;
 
@@ -68,7 +65,6 @@ export class Slider {
             nextSlide.style.visibility = `visible`;
         })
 
-        // 받아온 root 태그에 append
         root.style.display = `flex`;
         root.style.justifyContent = `center`;
         root.appendChild(leftBtn);
